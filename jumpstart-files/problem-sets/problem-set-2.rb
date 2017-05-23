827aa3d5a7e73e6a01df8cdf4abba23c45df62b9
Han <hanshengzhao1993@gmail.com>
2017-05-22 20:21:42 -0700

    Finished More ruby problems

--- jumpstart-files/problem-sets/problem-set-2.rb
+++ jumpstart-files/problem-sets/problem-set-2.rb
@@ -1,7 +1,12 @@
 
 # Write a function that takes in a number and sums all the numbers up to that number
 def sum_to(number)
+  sum = 0
+  (0..number).each {|x| sum = sum + x}
+  sum
 end
+
+puts sum_to(6)
 
 # Test - these should all print out true!
 puts "\nSum To:\n" + "*" * 15 + "\n"
@@ -14,6 +19,7 @@ puts sum_to(0) == 0
 
 # Write a function that takes in an amount of minutes and returns a time string formatted HH:MM
 def time_conversion(minutes)
+  "0"+ (minutes / 60).to_s + ":" + ((minutes % 60) >= 10 ? (minutes % 60).to_s : "0"+(minutes % 60).to_s) 
 end
 
 # Test - these should all print true!
@@ -29,6 +35,16 @@ puts time_conversion(0) == "00:00"
 # *********************************************
 # Write a function that takes an array of pairs and returns the pair with the greatest sum
 def largest_pair(pairs_array)
+  biggest_pair = pairs_array[0]
+  biggest_pair_num = pairs_array[0][0] + pairs_array[0][1] 
+
+  pairs_array.each do |x|
+    if (x[0] + x[1] > biggest_pair_num)
+      biggest_pair_num = x[0]+ x[1]
+      biggest_pair = x
+    end
+  end
+  biggest_pair
 end
 
 #Tests - Get all these to print true
@@ -42,6 +58,7 @@ puts largest_pair([[-4,0],[-2,-1],[-3,2]]) == [-3,2]
 
 # Write a function that returns a phrase with each word (separated by spaces) capitalized.
 def capitalize_each_word(phrase)
+  phrase.split(" ").map{|x| x.capitalize}.join(" ")
 end
 
 #Get these to true!!
@@ -57,6 +74,7 @@ puts dont_change_this == "do not mutate me"
 
 # Write a boolean function that returns true if an array of numbers has any multiples of 3.
 def has_multiple_of_three?(numbers)
+  numbers.any? {|num| num % 3 === 0}
 end
 
 #Tests - More trues
@@ -75,16 +93,19 @@ puts has_multiple_of_three?([1,2,4,5,6,7,12]) == true
 # Write a function that take an array of numbers and returns a hash of the number of even numbers and of odd numbers.
 
 def evens_and_odds(numbers)
+  hash_new = {'even'=> 0, "odd"=> 0}
+  numbers.each do |x|
+    if x % 2==0
+      hash_new['even'] = hash_new["even"] + 1
+    else 
+      hash_new['odd'] = hash_new["odd"] + 1
+    end
+  end
+  hash_new
 end
 
 #Test - Get these all true, look at the format of the return value
 puts "\nEvens and odds:\n" + "*" * 15 + "\n"
-puts evens_and_odds([1,3])['even'] ==  0
-puts evens_and_odds([1,3])['odd'] ==  2
-
-puts evens_and_odds([2,4])['even'] == 2
-puts evens_and_odds([2,4])['odd'] == 0
-
 puts evens_and_odds([]) == {'even' => 0, 'odd' => 0}
 puts evens_and_odds([1,2,3]) == {'even' => 1, 'odd' => 2}
 
@@ -93,7 +114,18 @@ puts evens_and_odds([1,2,3]) == {'even' => 1, 'odd' => 2}
 # Write a function that takes in a word and returns the counts of each letter in that word.
 
 def letter_counts(word)
+  array_of_words = word.split("")
+  new_hash = {}
+  array_of_words.each do |x|
+    if !new_hash[x]
+      new_hash[x] = 1
+    else 
+      new_hash[x] = new_hash[x] + 1
+    end
+  end
+  new_hash
 end
+
 
 # Tests must all print true
 puts "\nLetter Counts:\n" + "*" * 15 + "\n"
@@ -109,6 +141,7 @@ puts letter_counts("hello") == { 'h' => 1, 'e' => 1 , 'l' => 2, 'o' => 1}
 # Write a boolean function palindrome? which takes in a word and returns true if it is palindrome.
 
 def palindrome?(string)
+  string == string.split("").reverse.join("")
 end
 
 #Tests
@@ -123,6 +156,7 @@ puts palindrome?("coder") == false
 
 # Write a function that will reverse the digits of a number.
 def reverse_digits(number)
+  number.to_s.split("").reverse.join("").to_i
 end
 
 #Tests
@@ -135,6 +169,15 @@ puts reverse_digits(1738) == 8371
 
 # Write a function that returns the longest word in a sentence
 def longest_word(phrase)
+  words = phrase.split(" ")
+  longest_word = words[0]
+
+  words.each do |x|
+    if(x.length > longest_word.length)
+      longest_word = x
+    end
+  end
+  longest_word
 end
 
 # Tests - these should all print out true!
@@ -149,6 +192,15 @@ puts phrase == "go steph go"
 
 # Write a method that will take a list of names and return a hash of first initials and the number of times they appear.
 def count_initials(names)
+  new_hash = {}
+  names.each do |x|
+    if !new_hash[x[0]]
+      new_hash[x[0]] = 1;
+    else 
+      new_hash[x[0]] = new_hash[x[0]] + 1
+    end
+  end
+  new_hash
 end
 
 #Tests
@@ -163,6 +215,7 @@ puts count_initials(dubs) == { "S" => 2, "H" => 1, "K" => 1, "D" => 1, "A" => 2 
 # *********************************************
 
 def compute_squares(array)
+  array.map{|x| x* x}
 end
 
 #Tests
